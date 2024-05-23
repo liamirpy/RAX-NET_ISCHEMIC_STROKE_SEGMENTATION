20d3fe8ceeeab77ad607a5a73e9ee0ea803526a9
Amir Mousavi <amousavi@login02.matricsi.u-picardie.fr>
2024-05-23 14:07:31 +0200

    Add K_Fold Lesion and Normal in Cross-validation

---
+++ Cross-Validation/K_Fold_Normal/axial_sagittal_coronal_k_fold_normal.py
@@ -0,0 +1,371 @@
+### This is LIAMIRPY from the land of loneliness. 
+### In this code we split the data for the K-fold validation for normal slice 
+
+
+
+
+
+
+
+
+'''
+            Approach:
+
+        1- First we read each fold for lesion (csv file )
+        2- Finding the subject that are in that folds 
+        3- Finding the normal slice in each subject
+        4- Adding the same number of normal slice near to lesion slice from same subject
+
+        ATTENTION:
+            - Not add the same slice from the subject to more than one fold (for ovelaping subject)
+
+
+'''
+
+
+import csv
+
+import numpy as np
+
+import pandas as pd 
+
+import random
+
+import nibabel as nib
+
+
+random.seed(42)
+
+
+'''
+
+min_axial 2
+max_axial 179
+
+min_sagittal 20
+max_sagittal 176
+
+
+min_coronal 27
+min_coronal 227
+
+
+
+
+'''
+
+
+number_of_folds=5
+
+
+train_folder_mask='../../Moving_Converting_Data/Train_3D_Data/mask'
+
+
+axial_folds_csv='../K_Fold_Lesion/Axial_K_Fold_CSV'
+
+sagittal_folds_csv='../K_Fold_Lesion/Sagittal_K_Fold_CSV'
+
+coronal_folds_csv='../K_Fold_Lesion/Coronal_K_Fold_CSV'
+
+
+
+
+def spliting_noramal_slice(axial_folds_csv,sagittal_folds_csv,coronal_folds_csv,train_folder_mask):
+
+
+
+    for axial_fold in range(1,number_of_folds+1):
+
+        print('fold',axial_fold)
+
+
+        subjects_in_this_fold=[]
+
+        number_of_slice_for_each_subject={}
+
+        normal_slice=[]
+
+        number_of_slice_in_lesion_fold=0
+
+
+
+        with open(f'{axial_folds_csv}/Axial_Lesion_fold_0{axial_fold}.csv', 'r') as csvfile:
+
+
+            reader = csv.reader(csvfile)
+
+
+            next(reader)
+
+
+            for row in reader:
+
+                number_of_slice_in_lesion_fold +=1
+
+                subject_name=row[0]
+
+                if subject_name not in subjects_in_this_fold:
+                    subjects_in_this_fold.append(subject_name)
+                
+                # try:
+                #     number_of_slice_for_each_subject[subject_name] +=1
+                # except:
+                #     number_of_slice_for_each_subject[subject_name] =1 
+        
+
+        ## Read each subject 
+                    
+
+                    
+        for subject in subjects_in_this_fold:
+
+
+            mask_nii_nii=nib.load(f'{train_folder_mask}/{subject}_ses-1_space-MNI152NLin2009aSym_label-L_desc-T1lesion_mask.nii.gz')
+            mask_nii = mask_nii_nii.get_fdata()
+
+            for ax in range(mask_nii.shape[2]):
+                single_mask_slice=mask_nii[:,:,ax]
+                single_mask_slice[single_mask_slice > 0] = 1
+
+                if sum(sum(single_mask_slice)) ==0:
+
+                    normal_slice.append({'subject_name':subject,'slice':ax})
+
+        
+
+        ## Choose the number of random from the normal slice and add to that fold 
+        random.shuffle(normal_slice)
+
+        num_elements_to_choose = number_of_slice_in_lesion_fold
+
+        # Choose elements randomly from different parts of the list
+        chosen_indices = random.sample(range(len(normal_slice)), num_elements_to_choose)
+        chosen_elements = [normal_slice[i] for i in chosen_indices]
+
+
+
+
+        ### save the csv file 
+
+
+
+    # CSV file path
+        csv_file_path = f'Axial_K_Fold_CSV/Axial_Normal_fold_0{axial_fold}.csv'
+
+        # Writing data to CSV file
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            fieldnames = chosen_elements[0].keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            # Writing header
+            writer.writeheader()
+
+            # Writing data
+            writer.writerows(chosen_elements)
+
+        print(f'Data has been written to {csv_file_path}.')
+
+
+
+
+
+
+
+
+
+    for coronal_fold in range(1,number_of_folds+1):
+
+        print('fold',coronal_fold)
+
+
+        subjects_in_this_fold=[]
+
+        number_of_slice_for_each_subject={}
+
+        normal_slice=[]
+
+        number_of_slice_in_lesion_fold=0
+
+
+
+        with open(f'{coronal_folds_csv}/Coronal_Lesion_fold_0{coronal_fold}.csv', 'r') as csvfile:
+
+
+            reader = csv.reader(csvfile)
+
+
+            next(reader)
+
+
+            for row in reader:
+
+                number_of_slice_in_lesion_fold +=1
+
+                subject_name=row[0]
+
+                if subject_name not in subjects_in_this_fold:
+                    subjects_in_this_fold.append(subject_name)
+                
+                # try:
+                #     number_of_slice_for_each_subject[subject_name] +=1
+                # except:
+                #     number_of_slice_for_each_subject[subject_name] =1 
+        
+
+        ## Read each subject 
+                    
+
+                    
+        for subject in subjects_in_this_fold:
+
+
+            mask_nii_nii=nib.load(f'{train_folder_mask}/{subject}_ses-1_space-MNI152NLin2009aSym_label-L_desc-T1lesion_mask.nii.gz')
+            mask_nii = mask_nii_nii.get_fdata()
+
+            for co in range(  mask_nii.shape[2] ):
+                single_mask_slice=mask_nii[:,co,:]
+                single_mask_slice[single_mask_slice > 0] = 1
+
+                if sum(sum(single_mask_slice)) ==0:
+
+                    normal_slice.append({'subject_name':subject,'slice':co})
+
+        
+
+        ## Choose the number of random from the normal slice and add to that fold 
+        random.shuffle(normal_slice)
+
+        num_elements_to_choose = number_of_slice_in_lesion_fold
+
+        # Choose elements randomly from different parts of the list
+        chosen_indices = random.sample(range(len(normal_slice)), num_elements_to_choose)
+        chosen_elements = [normal_slice[i] for i in chosen_indices]
+
+
+
+
+        ### save the csv file 
+
+
+
+    # CSV file path
+        csv_file_path = f'Coronal_K_Fold_CSV/Coronal_Normal_fold_0{coronal_fold}.csv'
+
+        # Writing data to CSV file
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            fieldnames = chosen_elements[0].keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            # Writing header
+            writer.writeheader()
+
+            # Writing data
+            writer.writerows(chosen_elements)
+
+        print(f'Data has been written to {csv_file_path}.')
+
+
+
+
+    for sagittal_fold in range(1,number_of_folds+1):
+
+        print('fold',sagittal_fold)
+
+
+        subjects_in_this_fold=[]
+
+        number_of_slice_for_each_subject={}
+
+        normal_slice=[]
+
+        number_of_slice_in_lesion_fold=0
+
+
+
+        with open(f'{sagittal_folds_csv}/Sagittal_Lesion_fold_0{sagittal_fold}.csv', 'r') as csvfile:
+
+
+            reader = csv.reader(csvfile)
+
+
+            next(reader)
+
+
+            for row in reader:
+
+                number_of_slice_in_lesion_fold +=1
+
+                subject_name=row[0]
+
+                if subject_name not in subjects_in_this_fold:
+                    subjects_in_this_fold.append(subject_name)
+                
+                # try:
+                #     number_of_slice_for_each_subject[subject_name] +=1
+                # except:
+                #     number_of_slice_for_each_subject[subject_name] =1 
+        
+
+        ## Read each subject 
+                    
+
+                    
+        for subject in subjects_in_this_fold:
+
+
+            mask_nii_nii=nib.load(f'{train_folder_mask}/{subject}_ses-1_space-MNI152NLin2009aSym_label-L_desc-T1lesion_mask.nii.gz')
+            mask_nii = mask_nii_nii.get_fdata()
+
+            for co in range(  mask_nii.shape[2] ):
+                single_mask_slice=mask_nii[:,co,:]
+                single_mask_slice[single_mask_slice > 0] = 1
+
+                if sum(sum(single_mask_slice)) ==0:
+
+                    normal_slice.append({'subject_name':subject,'slice':co})
+
+        
+
+        ## Choose the number of random from the normal slice and add to that fold 
+        random.shuffle(normal_slice)
+
+        num_elements_to_choose = number_of_slice_in_lesion_fold
+
+        # Choose elements randomly from different parts of the list
+        chosen_indices = random.sample(range(len(normal_slice)), num_elements_to_choose)
+        chosen_elements = [normal_slice[i] for i in chosen_indices]
+
+
+
+
+        ### save the csv file 
+
+
+
+    # CSV file path
+        csv_file_path = f'Sagittal_K_Fold_CSV/Sagittal_Normal_fold_0{sagittal_fold}.csv'
+
+        # Writing data to CSV file
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            fieldnames = chosen_elements[0].keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            # Writing header
+            writer.writeheader()
+
+            # Writing data
+            writer.writerows(chosen_elements)
+
+        print(f'Data has been written to {csv_file_path}.')
+
+
+
+    return 'Done'
+
+
+
+
+
+
+                
+spliting_noramal_slice(axial_folds_csv,sagittal_folds_csv,coronal_folds_csv,train_folder_mask)
---
+++ Cross-Validation/plot_lesion_information.py
@@ -0,0 +1,397 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+class plot_lesion_information:
+
+
+    def lesion_information_in_3D_Training(self,csv_file,ranges):
+
+        csv_file = csv_file
+        df = pd.read_csv(csv_file)
+
+        # Extract the desired column
+        column_name = 'sum_voxel'  
+
+        data = df[column_name] / 1000
+
+
+        range_counts = []
+        for start, end, color in ranges:
+            count = ((data >= start) & (data < end)).sum()
+            range_counts.append((start, end, color, count))
+    
+
+
+
+
+
+        plt.figure(figsize=(10, 10))
+
+        # Plot the violin plot
+        x_value = 0
+        sns.violinplot(x=[x_value] * len(data), y=data, color='darkcyan', fill=True,inner_kws=dict(box_width=5, whis_width=1, color=".1"))
+
+        # Plot the scatter plot with the same x-coordinate
+        for start, end, color,count in range_counts:
+            mask = (data >= start) & (data < end)
+            x_values = np.random.uniform(x_value - 0.35, x_value + 0.35, sum(mask))  # Add slight jitter
+            plt.scatter(x_values, data[mask],color=color, label=f'{start} to {end} (10^3) Voxels (N={count})',s=20,linewidths=0.5)
+
+        # Set the labels for the axes
+        plt.xlabel('Lesion Size Distribution in 80 Percent of 655 3D MRI Subjects',fontsize=20)
+        plt.ylabel('volume (10 ^3) Voxels', fontsize=20)
+
+        # Display y-axis scale values
+        plt.yticks()  # This will keep the y-axis scale values
+
+        # Add legend
+        plt.legend()
+
+        # plt.show()
+
+        plt.grid(True)
+
+        plt.savefig('Lesion_information_in_80_percent_of_665_3D_Subjects.png', dpi=400)
+
+
+
+
+    def lesion_information_in_3D_Fusion(self,csv_file,ranges):
+
+        csv_file = csv_file
+        df = pd.read_csv(csv_file)
+
+        # Extract the desired column
+        column_name = 'sum_voxel'  
+
+        data = df[column_name] / 1000
+
+
+        range_counts = []
+        for start, end, color in ranges:
+            count = ((data >= start) & (data < end)).sum()
+            range_counts.append((start, end, color, count))
+    
+
+
+
+
+
+        plt.figure(figsize=(10, 10))
+
+        # Plot the violin plot
+        x_value = 0
+        sns.violinplot(x=[x_value] * len(data), y=data, color='darkcyan', fill=True,inner_kws=dict(box_width=5, whis_width=1, color=".1"))
+
+        # Plot the scatter plot with the same x-coordinate
+        for start, end, color,count in range_counts:
+            mask = (data >= start) & (data < end)
+            x_values = np.random.uniform(x_value - 0.35, x_value + 0.35, sum(mask))  # Add slight jitter
+            plt.scatter(x_values, data[mask],color=color, label=f'{start} to {end} (10^3) Voxels (N={count})',s=20,linewidths=0.5)
+
+        # Set the labels for the axes
+        plt.xlabel('Lesion Size Distribution in 20 Percent of 655 3D MRI Subjects',fontsize=20)
+        plt.ylabel('volume (10 ^3) Voxels', fontsize=20)
+
+        # Display y-axis scale values
+        plt.yticks()  # This will keep the y-axis scale values
+
+        # Add legend
+        plt.legend()
+
+        # plt.show()
+
+        plt.grid(True)
+
+        plt.savefig('Lesion_information_in_20_Percent_of_665_3D_Subjects.png', dpi=400)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    def lesion_information_in_axial(self,csv_file,ranges):
+
+        csv_file = csv_file
+        df = pd.read_csv(csv_file)
+
+        # Extract the desired column
+        column_name = 'sum_voxel'  
+
+        data = df[column_name] 
+
+
+        range_counts = []
+        for start, end, color in ranges:
+            count = ((data >= start) & (data < end)).sum()
+            range_counts.append((start, end, color, count))
+    
+
+
+
+
+
+        plt.figure(figsize=(20, 10))
+
+        # Plot the violin plot
+        x_value = 1
+        sns.violinplot(x=[x_value] * len(data), y=data, color='darkcyan', fill=True,inner_kws=dict(box_width=5, whis_width=1, color=".1"))
+
+        # Plot the scatter plot with the same x-coordinate
+        for start, end, color,count in range_counts:
+            mask = (data >= start) & (data < end)
+            x_values = np.random.uniform(x_value - 0.1, x_value + 0.1, sum(mask))  # Add slight jitter
+            plt.scatter(x_values, data[mask],color=color, label=f'{start} to {end} Pixels (N={count})',s=20,linewidths=0.5)
+
+        # Set the labels for the axes
+        plt.xlabel('Lesion Size Distribution in Axial Plane of 80 Percent of 655 3D MRI Subjects',fontsize=20)
+        plt.ylabel('volume Pixels', fontsize=20)
+
+        # Display y-axis scale values
+        plt.yticks()  # This will keep the y-axis scale values
+
+        # Add legend
+        plt.legend()
+
+        # plt.show()
+
+        plt.grid(True)
+
+        plt.savefig('Lesion_information_in_Axial_Plane_of_80_Percent_of_665_3D_Subjects.png', dpi=400)
+
+
+
+    def lesion_information_in_sagittal(self,csv_file,ranges):
+
+        csv_file = csv_file
+        df = pd.read_csv(csv_file)
+
+        # Extract the desired column
+        column_name = 'sum_voxel'  
+
+        data = df[column_name] 
+
+
+        range_counts = []
+        for start, end, color in ranges:
+            count = ((data >= start) & (data < end)).sum()
+            range_counts.append((start, end, color, count))
+    
+
+
+
+
+
+        plt.figure(figsize=(20, 10))
+
+        # Plot the violin plot
+        x_value = 1
+        sns.violinplot(x=[x_value] * len(data), y=data, color='darkcyan', fill=True,inner_kws=dict(box_width=5, whis_width=1, color=".1"))
+
+        # Plot the scatter plot with the same x-coordinate
+        for start, end, color,count in range_counts:
+            mask = (data >= start) & (data < end)
+            x_values = np.random.uniform(x_value - 0.1, x_value + 0.1, sum(mask))  # Add slight jitter
+            plt.scatter(x_values, data[mask],color=color, label=f'{start} to {end} Pixels (N={count})',s=20,linewidths=0.5)
+
+        # Set the labels for the axes
+        plt.xlabel('Lesion Size Distribution in Sagittal Plane of 80 Percent of 655 3D MRI Subjects',fontsize=20)
+        plt.ylabel('volume Pixels', fontsize=20)
+
+        # Display y-axis scale values
+        plt.yticks()  # This will keep the y-axis scale values
+
+        # Add legend
+        plt.legend()
+
+        # plt.show()
+
+        plt.grid(True)
+
+
+
+        plt.savefig('Lesion_information_in_Sagittal_Plane_in_80_Percent_of_665_3D_Subjects.png', dpi=400)
+
+
+    def lesion_information_in_coronal(self,csv_file,ranges):
+
+ 
+        csv_file = csv_file
+        df = pd.read_csv(csv_file)
+
+        # Extract the desired column
+        column_name = 'sum_voxel'  
+
+        data = df[column_name] 
+
+
+        range_counts = []
+        for start, end, color in ranges:
+            count = ((data >= start) & (data < end)).sum()
+            range_counts.append((start, end, color, count))
+    
+
+
+
+
+
+        plt.figure(figsize=(20, 10))
+
+        # Plot the violin plot
+        x_value = 1
+        sns.violinplot(x=[x_value] * len(data), y=data, color='darkcyan', fill=True,inner_kws=dict(box_width=5, whis_width=1, color=".1"))
+
+        # Plot the scatter plot with the same x-coordinate
+        for start, end, color,count in range_counts:
+            mask = (data >= start) & (data < end)
+            x_values = np.random.uniform(x_value - 0.1, x_value + 0.1, sum(mask))  # Add slight jitter
+            plt.scatter(x_values, data[mask],color=color, label=f'{start} to {end} Pixels (N={count})',s=20,linewidths=0.5)
+
+        # Set the labels for the axes
+        plt.xlabel('Lesion Size Distribution in Coronal Plane of 80 Percent of 655 3D MRI Subjects',fontsize=20)
+        plt.ylabel('volume Pixels', fontsize=20)
+
+        plt.yticks()  # This will keep the y-axis scale values
+
+        plt.legend()
+
+
+        plt.grid(True)
+
+        plt.savefig('Lesion_information_in_Coronal_Plane_of_80_Percent_of_665_3D_Subjects.png', dpi=400)
+
+
+
+
+
+
+ranges_in_3D_Subject = [
+    (0, 5, 'r'), 
+    (5, 30, 'indigo'), 
+    (30, 60, 'gold'), 
+    (60, 100, 'b'), 
+    (100, 200, 'chocolate'), 
+    (200, float('inf'), 'black'), 
+]
+
+
+
+ranges_in_axial_3D_Subject = [
+    (0, 200, 'r'), 
+    (200, 500, 'indigo'), 
+    (500, 1000, 'gold'), 
+    (1000, 2000, 'b'), 
+    (2000, 3000, 'chocolate'), 
+    (3000, float('inf'), 'black'), 
+]
+
+
+
+
+
+ranges_in_Sagittal_3D_Subject = [
+    (0, 1000, 'r'), 
+    (1000, 2000, 'indigo'), 
+    (2000, 3000, 'gold'), 
+    (3000, 5000, 'b'), 
+    # (5000, 7000, 'chocolate'), 
+    (5000, float('inf'), 'black'), 
+    
+]
+
+
+
+
+ranges_in_Coronal_3D_Subject = [
+    (0, 200, 'r'), 
+    (200, 500, 'indigo'), 
+    (500, 1000, 'gold'), 
+    (1000, 2000, 'b'), 
+    (2000, 3000, 'chocolate'), 
+    (3000, float('inf'), 'black'), 
+]
+
+
+
+
+
+
+
+lesion_3D_subject_csv='./CSV/Lesion_information_for_80_percent_of_3D_MRI_Subject.csv'
+
+
+plot=plot_lesion_information()
+
+plot.lesion_information_in_3D_Training(lesion_3D_subject_csv,ranges_in_3D_Subject)
+
+
+
+
+
+lesion_3D_subject_csv='./CSV/Lesion_information_for_20_percent_of_3D_MRI_Subject.csv'
+
+
+plot=plot_lesion_information()
+
+plot.lesion_information_in_3D_Fusion(lesion_3D_subject_csv,ranges_in_3D_Subject)
+
+
+
+
+
+axial_csv='./CSV/Axial_Lesion_information_for_80_percent_of_3D_MRI_Subject.csv'
+
+
+
+plot=plot_lesion_information()
+
+plot.lesion_information_in_axial(axial_csv,ranges_in_axial_3D_Subject)
+
+
+
+
+
+
+
+sagittal_csv='./CSV/Sagittal_Lesion_information_for_80_percent_of_3D_MRI_Subject.csv'
+
+
+
+plot=plot_lesion_information()
+
+plot.lesion_information_in_sagittal(sagittal_csv,ranges_in_Sagittal_3D_Subject)
+
+
+
+
+
+coronal_csv='./CSV/Coronal_Lesion_information_for_80_percent_of_3D_MRI_Subject.csv'
+
+
+
+plot=plot_lesion_information()
+
+plot.lesion_information_in_coronal(coronal_csv,ranges_in_Coronal_3D_Subject)
+
+
